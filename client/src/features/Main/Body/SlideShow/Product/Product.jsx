490982baa38ae0db2af37c2b231c36bef15447d5
goutham123gaddam <gaddamgoutham123@gmail.com>
2023-05-03 00:36:51 +0530

    mapping of mulitlpe products added

--- client/src/features/Main/Body/SlideShow/Product/Product.jsx
+++ client/src/features/Main/Body/SlideShow/Product/Product.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import './Product.css';
 
-function Product() {
+function Product(props) {
 
     return (
         <>
             <div className="container">
-                <div className="discount">Save $20</div>
+                <div className="discount">Save ${props.discount}</div>
                 <div className="image">
-                    <img src="https://ee.co.uk/content/dam/ee-homepage/Deals/S23-Ultra_237x237.png.eeimg.400.1-1.default.jpg" alt="img"></img>
+                    <img src={props.img} alt="img"></img>
                 </div>
                 <div className="details">
-                    <div className="speciality-container"><div className="speciality">Unlimited Data</div></div>
-                    <div className="company">Samsung Galaxy</div>
-                    <div className="model">S23 Ultra</div>
-                    <div className="price">$30</div>
+                    <div className="speciality-container"><div className="speciality">{props.speciality}</div></div>
+                    <div className="company">{props.company}</div>
+                    <div className="model">{props.model}</div>
+                    <div className="price">${props.price}</div>
                     <div className="shop-button-container"><div className="shop-button">Shop deal</div></div>
                 </div>
             </div>
--- client/src/features/Main/Body/SlideShow/Slideshow.jsx
+++ client/src/features/Main/Body/SlideShow/Slideshow.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Slideshow.css';
 import Product from './Product/Product';
+import Strings from '../../../../Strings/strings.json';
 
 function Slideshow() {
 
     return (
         <>
-            <div>
-                <Product />
+            <div className='slideshow'>
+                {Strings.latest.map((obj) => {
+                    return (
+                        <Product 
+                            discount={obj.discount}
+                            img={obj.img}
+                            speciality={obj.speciality}
+                            company={obj.company}
+                            model={obj.model}
+                            price={obj.price}
+                        />
+                    )
+                })}
             </div>
         </>
     )
