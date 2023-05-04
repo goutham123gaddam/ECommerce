9d323c7993df31bf8066014fdb99b41311081f78
goutham123gaddam <gaddamgoutham123@gmail.com>
2023-05-04 15:52:51 +0530

    Footer added

---
+++ client/src/features/Main/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+
+    return (
+        <>
+            <div className="foot1">
+                <div>About us</div>
+                <div>Business</div>
+                <div>Partnerships</div>
+            </div>
+            <div className="foot2">
+                <div>c 2023 NetStoneGlobal</div>
+                <div>netstoneglobal@gmail.com</div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
--- client/src/features/Main/Main.jsx
+++ client/src/features/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from './Header/Header';
 import './Main.css';
 import Body from './Body/Body'
+import Footer from './Footer/Footer';
 
 export function Main() {
     
@@ -9,6 +10,7 @@ export function Main() {
         <div className='body'>
             <Header />
             <Body />
+            <Footer />
         </div>
     )
 }
