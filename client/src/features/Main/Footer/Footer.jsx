6d924f5084452f5d49d729227465cc9e9cf59632
goutham123gaddam <gaddamgoutham123@gmail.com>
2023-05-04 16:17:34 +0530

    Footer icons added

--- client/src/features/Main/Footer/Footer.jsx
+++ client/src/features/Main/Footer/Footer.jsx
@@ -9,7 +9,17 @@ function Footer() {
                 <div>About us</div>
                 <div>Business</div>
                 <div>Partnerships</div>
+                <div>Privacy</div>
+                <div>Terms and Conditions</div>
             </div>
+            <div className="icons">
+                <div>
+                <img src={process.env.PUBLIC_URL+'/icons/icons8-facebook-50.png'}></img>
+                <img src={process.env.PUBLIC_URL+'/icons/icons8-twitter-50.png'}></img>
+                <img src={process.env.PUBLIC_URL+'/icons/icons8-instagram-50.png'}></img>
+                </div>
+            </div>
+            <div className="divider"></div>
             <div className="foot2">
                 <div>c 2023 NetStoneGlobal</div>
                 <div>netstoneglobal@gmail.com</div>
