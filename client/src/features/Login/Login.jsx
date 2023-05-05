e4c730f5db0d97bdc441bc4978c1b56e93273eef
goutham123gaddam <gaddamgoutham123@gmail.com>
2023-05-05 14:13:35 +0530

    login page added

--- client/src/features/Login/Login.jsx
+++ client/src/features/Login/Login.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import {Header} from '../Main/Header/Header';
+import './Login.css'
 
 function Login() {
 
     return (
         <>
             <Header isHome={false}/>
-            <div>Login</div>
+            <div className="login-container">
+                <div className="login-title">Login</div>
+                <div className="input-container">
+                    <div className="title">Email</div>
+                    <input placeholder="youremail@gmail.com"/>
+                </div>
+                <div className="input-container">
+                    <div className="title">Password</div>
+                    <input placeholder="*********"/>
+                </div>
+                <div className="submit-button">Submit</div>
+            </div>
         </>
     )
 }
